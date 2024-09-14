ed364cb11f018a5847272d4b9890e373c8ae9b51
Pietro Urbina <urbinapala2005@gmail.com>
2024-09-14 15:01:00 -0500

    Solo se acoplo a los demas componentes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-// src/App.js
 import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// Importamos los componentes
-import CompShowUsers from './components/users/ShowUsers';
+// Importamos los componentes de las tablas
+import CompShowUsers from './components/users/showUsers';
 import CompCreateUsers from './components/users/createUsers';
-import CompEditUsers from "./components/users/EditUsers";
+import CompEditUsers from './components/users/editUsers';
 
 import CompShowBox from './components/box/showBox';
 import CompCreateBox from './components/box/createBox';
 import CompEditBox from "./components/box/editBox";
 
-import CompshowProducts from './components/products/showProducts';
-import CompcreateProducts from './components/products/createProducts';
-import CompeditProducts from "./components/products/editProducts";
+import CompShowProducts from './components/products/showProducts';
+import CompCreateProducts from './components/products/createProducts';
+import CompEditProducts from './components/products/editProducts';
 
+import CompShowCustomers from './components/customers/showCustomers';
+import CompCreateCustomers from './components/customers/createCustomers';
+import CompEditCustomers from './components/customers/editCustomers';
+
+import Dashboard from './components/dasboards/repostesDashboard';
 import Sidebar from './components/Home/Sidebar';
 
 function App() {
-  const [isCollapsed, setIsCollapsed] = useState(false);
 
-  // Función para alternar el estado del sidebar
-  const toggleSidebar = () => {
-    setIsCollapsed(!isCollapsed);
-  };
+  const [isCollapsed] = useState(true);
 
   return (
     <div className="App">
@@ -32,21 +32,32 @@ function App() {
         {/* Sidebar que puede colapsarse */}
         <Sidebar isCollapsed={isCollapsed} />
 
-        {/* Navbar que controla el sidebar */}c
+        {/* Definir las rutas para cada tabla */}
         <div className={`main-content ${isCollapsed ? 'collapsed' : ''}`}>
-          <Routes> */
-            {/* <Route path='/' element={<CompShowUsers />} />
-            <Route path='/create' element={<CompCreateUsers />} />
-            <Route path='/edit/:id' element={<CompEditUsers />} /> */}
+          <Routes>
+            {/* Ruta para el Dashboard en Inicio */}
+            <Route path="/Home" element={<Dashboard />} />
 
-            {/* <Route path='/' element={<CompShowBox />} />
-            <Route path='/create' element={<CompCreateBox />} />
-            <Route path='/edit/:id' element={<CompEditBox />} /> */}
+            {/* Rutas para Usuarios */}
+            <Route path='/usuarios' element={<CompShowUsers />} />
+            <Route path='/usuarios/create' element={<CompCreateUsers />} />
+            <Route path='/usuarios/edit/:id' element={<CompEditUsers />} />
 
-            <Route path='/' element={<CompshowProducts />} />
-            <Route path='/create' element={<CompcreateProducts />} />
-            <Route path='/edit/:id' element={<CompeditProducts />} />
-            
+            {/* Rutas para Box */}
+            <Route path='/box' element={<CompShowBox />} />
+            <Route path='/box/create' element={<CompCreateBox />} />
+            <Route path='/box/edit/:id' element={<CompEditBox />} />
+
+            {/* Rutas para Productos */}
+            <Route path='/productos' element={<CompShowProducts />} />
+            <Route path='/productos/create' element={<CompCreateProducts />} />
+            <Route path='/productos/edit/:id' element={<CompEditProducts />} />
+
+            {/* Rutas para Clientes */}
+            <Route path='/clientes' element={<CompShowCustomers />} />
+            <Route path='/clientes/create' element={<CompCreateCustomers />} />
+            <Route path='/clientes/edit/:id' element={<CompEditCustomers />} />
+
           </Routes>
         </div>
       </BrowserRouter>
